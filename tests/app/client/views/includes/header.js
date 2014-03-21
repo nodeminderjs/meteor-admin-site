24b01a2a0e96b836a29171ea2b4b93d330933e09
NodeMinderJS <nodeminderjs@gmail.com>
2014-03-21 15:16:04 +0000

    Sensitive nav menu itens Home and Admin

--- tests/app/client/views/includes/header.js
+++ tests/app/client/views/includes/header.js
@@ -2,8 +2,7 @@ Template.header.helpers({
   canAdmin: function() {
     return (Meteor.user() && Meteor.user().profile.role != 'user');
   },
-  adminRoute: function() {
-    var basePath = Admin.basePath;
-    
+  isAdminRoute: function() {
+    return Admin.isAdminRoute();
   }
 });
